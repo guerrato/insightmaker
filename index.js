773df6bca64c419493ee2d557177d49d59057664
Pedro Guerrato <pguerrato@gmail.com>
2021-02-04 18:46:21 -0300

    Implemented load module functions and run model

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const api = require('./api')
 
-const loadModel = model => {
-  try {
-    return api.loadXML(model)
-  } catch (err) {
-    throw err
-  }
+const loadModelFromFile = async file => {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader()
+    reader.readAsText(file, "UTF-8")
+    reader.onload = async (evt) => {
+      resolve(await loadModel(evt.target.result))
+    }
+    reader.onerror = function (err) {
+      reject(err)
+    }
+  })
 }
 
-const execModel = model => {
-  try {
-    return api.runModel({ silent: model })
-  } catch (err) {
-    throw err
-  }
+const loadModel = async model => {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(api.loadXML(model))
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+const execModel = async model => {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(api.runModel({ silent: model }))
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
 module.exports = {
+  loadModelFromFile,
   loadModel,
   execModel
 }
